475ae77a89655e68c46b67b181127ef24af97c26
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2022-03-24 09:54:05 -0300

    Finalizando a verificação dos inputs de criação e preparando um backend fake com o miragejs

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -19,7 +19,9 @@ type CreateUserFormData = {
     nome: yup.string().required("Nome obrigatório"),  
     email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
     password: yup.string().required("Senha Obrigatório").min(6, 'No mínimo 6 caracteres'),
-    password_confirmation: yup.string().required("Senha Obrigatório")
+    password_confirmation: yup.string().oneOf([
+        null, yup.ref('password')
+    ], 'As senhas precisam ser iguais')
   })
   
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
@@ -40,20 +42,20 @@ export default function CreateUser(){
             <Flex w="100%" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} onSubmit={handleSubmit(handleCreateUser)}>
+                <Box flex="1" as="form" borderRadius={8} bg="gray.800" p={["6", "8"]} onSubmit={handleSubmit(handleCreateUser)} >
                     <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
                     <Divider my="6" borderColor="gray.700"/>
 
                     <VStack spacing="8">
                         <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-                            <Input name="name" label="Nome completo" error={ errors.name }/>
-                            <Input name="email" type="email" label="E-mail"/>
+                            <Input name="name" label="Nome completo" error={ errors.nome } {...register("nome")}/>
+                            <Input name="email" type="email" label="E-mail" error={ errors.email } {...register("email")}/>
                         </SimpleGrid>
 
                         <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-                            <Input name="password" type="password" label="Senha"/>
-                            <Input name="password_confirmation" type="password" label="Confirmação da senha"/>
+                            <Input name="password" type="password" label="Senha" error={ errors.password } {...register("password")}/>
+                            <Input name="password_confirmation" type="password" label="Confirmação da senha" error={ errors.password_confirmation } {...register("password_confirmation")}/>
                         </SimpleGrid>
                     </VStack>
                     <Flex mt="8" justify="flex-end">
@@ -61,7 +63,7 @@ export default function CreateUser(){
                         <Link href="/users" passHref>
                             <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
                         </Link>
-                            <Button colorScheme="pink">Enviar</Button>
+                            <Button type="submit" colorScheme="pink" isLoading={formState.isSubmitting}>Enviar</Button>
                         </HStack>
                     </Flex>
                 </Box>    
