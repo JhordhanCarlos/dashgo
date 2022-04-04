45a599f587f3d5ef0d4acf5441f74ad2e00fa583
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2022-04-04 09:42:57 -0300

    Concertando a paginação e o hook de usuários

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,110 +1,125 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import NextLink from "next/link";
+import { Box, Flex, Button, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Td, Tbody, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import {useQuery} from 'react-query'
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList(){
-    const { data, isLoading, isFetching, error } = useUsers()
+export default function UserList() {
+    const [ page, setPage ] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page);
 
     const isWideVersion = useBreakpointValue({
         base: false,
-        lg:true
+        lg: true
     })
 
-    useEffect(() =>{
-        fetch('http://localhost:3000/api/users')
-        .then(response => response.json())
-        .then(data => console.log(data))
-    },[])
+    async function handlePrefetchUser(userId: string) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
 
-    return(
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10, //10 minutos 
+        })
+    }
+
+    return (
         <Box>
             <Header />
-            
-            <Flex w="100%" maxWidth={1480} mx="auto" px="6">
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                    {!isLoading && isFetching && <Spinner color="gray.500"/>}
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
+                        </Heading>
 
-                        <Link href="/users/create" passHref>
+                        <NextLink href="/users/create" passHref>
                             <Button 
                                 as="a" 
                                 size="sm" 
                                 fontSize="sm" 
-                                colorScheme="pink" 
-                                leftIcon={<Icon as={RiAddLine} fontSize="26"/>}
-                                >
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="26" />}
+                            >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
-                    {isLoading ? (
+                    { isLoading ? (
                         <Flex justify="center">
                             <Spinner />
                         </Flex>
-                       )
-                    : 
-                        error ? (
-                            <Flex justify="center">
-                                <Text>Falha ao obeter os dados</Text>
-                            </Flex>
-                        ): (
-                            <>
-                                <Table colorScheme="whiteAlpha">
-                                    <Thead>
-                                        <Tr>
-                                            <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                                <Checkbox colorScheme="pink" />
-                                            </Th>
-                                            <Th>Usuários</Th>
-                                            {isWideVersion && <Th>Data de Cadastro</Th>}
-                                            <Th width="8"></Th>
-                                        </Tr>
-                                    </Thead>
-                                    <Tbody>
-                                        {data.map(user => {
-                                            return (
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter os dados do usuário</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        { isWideVersion && <Th>Data de cadastro</Th> }
+                                        <Th width="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.users.map(user => {
+                                        return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
                                                     <Checkbox colorScheme="pink" />
                                                 </Td>
                                                 <Td>
-                                                    <Text fontWeight="bold">{user.name}</Text>
-                                                    <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    <Box>
+                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
+                                                        
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
                                                 </Td>
-                                                {isWideVersion && <Td> {user.createdAt} </Td>}
-                                                {/* <Td> 
-                                                    <Button 
-                                                        as="a" 
-                                                        size="sm" 
-                                                        fontSize="sm" 
-                                                        colorScheme="purple" 
-                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                                        >
-                                                        Editar
-                                                    </Button>
-                                                </Td> */}
+                                                { isWideVersion && <Td>{user.createdAt}</Td> }
                                             </Tr>
-                                            )
-                                        })}
-                                    </Tbody>
-                                </Table>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
 
-                                <Pagination />
-                            </>
-                        )
-                    }
-                </Box>    
+                            <Pagination 
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
+                        </>
+                    )}
+                </Box>
             </Flex>
         </Box>
-    )
+    );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//     const { users, totalCount } = await getUsers(1)
+    
+//     return {
+//         props: {
+//             users,
+//         }
+//     }
+// }
--- dashgo/src/services/hooks/useUsers.ts
+++ dashgo/src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { api } from "../api";
 
 type User = {
@@ -8,29 +8,42 @@ type User = {
     createdAt: string;
 }
 
-async function getUsers(): Promise<User[]>{
-   
-        const {data} = await api.get('/users')
-        
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            };
-        });
-
-        return users;
-    
+type GetUserResponse = {
+    totalCount: number;
+    users: User[];
 }
 
-export function useUsers(){
-    return useQuery('users', getUsers, {
-        staleTime : 1000 * 5
+export async function getUsers(page: number): Promise<GetUserResponse> {
+    const { data, headers} = await api.get('users', {
+        params: {
+            page,
+        }
+    })
+
+    const totalCount = Number(headers['x-total-count'])
+
+    const users = data.users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+    return {
+        users,
+        totalCount,
+    };
+}
+
+export function useUsers(page: number, options?: GetUserResponse) {
+    return useQuery(['users', page], () => getUsers(page), {
+        staleTime: 1000 * 60 * 10, //10 minutos
+        ...options,
     })
 }
